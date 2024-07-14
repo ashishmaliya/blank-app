c02e6bec84aca8f49ddd09f6915aa8b96edb054f
ashishmaliya <ashishmmaliya.domain@gmail.com>
2024-07-14 21:37:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
 import streamlit as st
+from newspaper import Article
 
 def scrape_article(url):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
-
+        article = Article(url)
+        article.download()
+        article.parse()
+        
         # Extract title
-        title = soup.find('h1').text if soup.find('h1') else 'N/A'
+        title = article.title
 
-        # Attempt to find author by various common tags and attributes
-        author = None
-        
-        # Common author class names
-        author_tags = soup.find_all(class_='author') + soup.find_all(class_='author-name') + soup.find_all(class_='byline')
-        for tag in author_tags:
-            if tag.text:
-                author = tag.text.strip()
-                break
-        
-        # Meta tags
-        if not author:
-            meta_author = soup.find('meta', attrs={'name': 'author'})
-            if meta_author and meta_author.get('content'):
-                author = meta_author['content'].strip()
+        # Extract author
+        authors = ', '.join(article.authors) if article.authors else 'N/A'
 
-        # Byline links
-        if not author:
-            byline_link = soup.find('a', class_='byline')
-            if byline_link and byline_link.text:
-                author = byline_link.text.strip()
-
-        if not author:
-            author = 'N/A'
-
-        # Combine all paragraphs to get content
-        content = ' '.join(p.text for p in soup.find_all('p'))
+        # Extract content
+        content = article.text
 
         return {
             'title': title,
-            'author': author,
+            'author': authors,
             'content': content,
             'url': url
         }
